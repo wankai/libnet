7c7c3a1ade3f6e32b5747d1b72e630c55ce82a1e
Wankai Zhang <wankaizhang@gmail.com>
2016-04-19 13:58:52 +0800

    Update Task.h

--- Task.h
+++ Task.h
@@ -8,4 +8,18 @@ class Task
   virtual void run() = 0;
 };
 
+class LambdaTask : public Task
+{
+ public
+  override void run()
+  {
+    
+  }
+};
+
+template<Fun, Arg...>
+auto make_lambda(Fun&& f, Arg... args)
+{
+  return std::make_unique<
+}
 } // namespace net
