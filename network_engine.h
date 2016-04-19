5984c1847bee543bff5b8eda9051703752509bbf
Wankai Zhang <wankaizhang@gmail.com>
2016-04-19 14:34:11 +0800

    Update network_engine.h

--- network_engine.h
+++ network_engine.h
@@ -9,4 +9,15 @@ class network_engine
  private:
 };
 
+class engine_builder_t
+{
+ public:
+  void thread_num(int num);
+  
+  std::unique_ptr<network_engine> build();
+};
+std::unique_ptr<engine_builder_t> engine_builder()
+{
+  return std::make_unique<engine_builder_t>();
+}
 } // namespace net
