5c40e3bca82ee16fa237e5e28a34fa4c8f7fb711
Wankai Zhang <wankaizhang@gmail.com>
2016-04-19 13:44:03 +0800

    Update InetAddress.h

--- InetAddress.h
+++ InetAddress.h
@@ -1,1 +1,13 @@
 
+#progma once
+
+class InetAddress
+{
+ public:
+  InetAddress(int port);
+  InetAddress(const string& ip, int port);
+  InetAddress(IPAddress ip, int port);
+ private:
+  IPAddress ip_;
+  int       port_;
+};
